039ac59d74543c10d63a7a0f10195b2a40b3516d
Ibrahim Gragirena <ibrach98@gmail.com>
2022-08-02 12:29:46 -0300

    Querys menu mobile and desktop

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
+// Toggle Menus Desktop & Mobile
+
 let menuEmail = document.querySelector('.navbar-email');
 let desktopMenu = document.querySelector('.desktop-menu')
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
+let menuBurguer = document.querySelector('.menu')
+let mobileMenu = document.querySelector('.mobile-menu')
 
-function toggleDesktopMenu() {
+menuEmail.addEventListener('click', toggleMenus);
+menuBurguer.addEventListener('click', toggleMenus);
+
+function toggleMenus() {
     desktopMenu.classList.toggle('inactive');
+    mobileMenu.classList.toggle('inactive');
+
 }
+
