69b459e2f617e48d159cf1ee80929abfdb850ed2
Ibrahim Gragirena <ibrach98@gmail.com>
2022-08-02 23:19:46 -0300

    Toggle menus and shopping cart display

--- main.js
+++ main.js
@@ -6,12 +6,27 @@ let desktopMenu = document.querySelector('.desktop-menu')
 let menuBurguer = document.querySelector('.menu')
 let mobileMenu = document.querySelector('.mobile-menu')
 
+//  Shopping Cart
+
+let menuCart = document.querySelector('.navbar-shopping-cart')
+let aside = document.querySelector('.product-detail')
+
+// Listeners 
 menuEmail.addEventListener('click', toggleMenus);
 menuBurguer.addEventListener('click', toggleMenus);
+menuCart.addEventListener('click', toggleCartMenu)
+
+//Functions 
 
 function toggleMenus() {
     desktopMenu.classList.toggle('inactive');
     mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 
 }
 
+function toggleCartMenu() {
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
+}
